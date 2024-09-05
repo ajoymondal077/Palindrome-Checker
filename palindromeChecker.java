5f49cfdaee22c2c04676ffab0f07fec0344aba1e
ajoymondal077 <mondalajoy077@gmail.com>
2024-09-05 19:34:38 +0530

    compile project

--- palindromeChecker.java
+++ palindromeChecker.java
@@ -1,4 +1,19 @@
 import java.util.*;
 class palindromeChecker{
-    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String to check if it is a palindrome:");
+        String original = sc.nextLine();
+        String cleaned = original.replaceAll("[^a-zA-Z]","").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+        if(cleaned.equals(reversed))
+        {
+            System.out.println("The String is Palindrome");
+        }
+        else
+        {
+            System.out.println("The String is not a Palindrome");
+        }
+        sc.close();
+    } 
 }
